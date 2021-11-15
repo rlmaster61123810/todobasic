3fece4c24aebe6ae2b8ae0050403d301cac1999e
The new begin <61123810@g.cmru.ac.th>
2021-11-15 16:51:51 +0700

    first commit

---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,12 @@
+<html>
+
+<head>
+    <title> Todo App demo</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"/>
+</head>
+
+<body>
+    <h1>Task list</h1>
+</body>
+
+</html>
