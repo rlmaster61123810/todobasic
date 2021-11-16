cddafdcd5e326de18c0d0d0ca33473655f1494f4
The new begin <61123810@g.cmru.ac.th>
2021-11-16 21:02:43 +0700

    indexlayout

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,12 +1,7 @@
-<html>
+@extends('layouts.app')
 
-<head>
-    <title> Todo App demo</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"/>
-</head>
+@section('content')
 
-<body>
     <h1>Task list</h1>
-</body>
 
-</html>
+@endsection
