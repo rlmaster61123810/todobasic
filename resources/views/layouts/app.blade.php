a14aa1192e189fb4dedb192ac5155d7814b0e254
The new begin <61123810@g.cmru.ac.th>
2021-11-16 21:12:23 +0700

    bootaps All tasks

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,10 +5,27 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" />
 </head>
 
-    <body>
-        <div class="container">
-        @yield('content')
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link active" aria-current="page" href="/">All tasks</a>
+                    <a class="nav-link" href="/tasks/create">New tasks</a>
+
+                </div>
+            </div>
         </div>
-    </body>
+    </nav>
+    <div class="container">
+
+        @yield('content')
+    </div>
+</body>
 
 </html>
