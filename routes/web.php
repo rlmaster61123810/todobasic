25af8c84fa383072a6dd5f1d5abdcdfda88c207d
The new begin <61123810@g.cmru.ac.th>
2021-11-16 22:36:34 +0700

    Taskrount

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('tasks.index');
-});
+Route::get('/', 'TasksController@index');
 
 Route::get('/tasks/create', function () {
     return view('tasks.create');
